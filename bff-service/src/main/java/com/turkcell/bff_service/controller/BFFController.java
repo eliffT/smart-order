fca58e669cc38cf40e4d9a69c9997045c00e1e3e
eliffT <elifftasdemirr@gmail.com>
2025-10-31 23:19:06 +0300

    add launch configuration for NotificationService and clean up application.yml in order-service

--- bff-service/src/main/java/com/turkcell/bff_service/controller/BFFController.java
+++ bff-service/src/main/java/com/turkcell/bff_service/controller/BFFController.java
@@ -10,7 +10,6 @@ import java.util.Map;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/auth")
---
+++ notification-service/src/main/java/com/turkcell/notification_service/messaging/consumer/OrderCreatedNotification.java
@@ -0,0 +1,20 @@
+package com.turkcell.notification_service.messaging.consumer;
+
+import java.util.function.Consumer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OrderCreatedNotification {
+    @Bean
+    public Consumer<OrderCreatedEvent> orderCreated() {
+        return event -> {
+            System.out.println("[Notification] maili gönderildi.");
+            System.out.println(event.email);
+        };
+    }
+
+    record OrderCreatedEvent(String productId, String orderId, String email) {
+    }
+}
